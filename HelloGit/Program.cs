15c998570aa9ca078dad76cfedb5cfc6828a74c3
aharkleroad <arh4811@g.rit.edu>
2024-09-03 16:36:28 -0400

    HelloGit PE

---
+++ HelloGit/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace HelloGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Git!");
+            Console.WriteLine("This is Aizlynn Harkleroad.");
+        }
+    }
+}
