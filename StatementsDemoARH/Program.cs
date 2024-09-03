311fd761fbf4aa9e581e6a99750f95db7b170718
aharkleroad <arh4811@g.rit.edu>
2024-09-03 16:26:29 -0400

    Visual Studio file imports

---
+++ StatementsDemoARH/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace StatementsDemoARH
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This will go away!");
+            Console.Clear();
+
+            Console.Write("Hi ");
+            Console.Write("there ");
+            Console.Write("everybody!");
+
+            Console.WriteLine("Hi " + "there " + "everybody!");
+
+            Console.WriteLine("Hi there everybody!");
+
+            Console.WriteLine(6 + 3 * 8 - 4);
+            Console.WriteLine(3.14159 * 8.2 * 8.2);
+
+            Console.WriteLine("Hours per week: " + 7 * 24);
+            Console.WriteLine("5" + 5);
+        }
+    }
+}
