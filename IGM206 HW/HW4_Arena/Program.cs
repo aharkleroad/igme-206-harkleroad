a2ca9c6b9052c14d3e4061bac47bba284ed539ef
aharkleroad <arh4811@g.rit.edu>
2024-10-08 00:34:27 -0500

    Preliminary HW4 files, not completed

---
+++ IGM206 HW/HW4_Arena/Program.cs
@@ -0,0 +1,282 @@
+﻿/***
+ * Aizlynn Harkleroad
+ * 
+ * HW 4 - The Arena
+ * Write-up: https://docs.google.com/document/d/15Rl0oXwNXdGze8p5HcrZ8n4y78oiubW5pbkJei2YTPI/edit?usp=sharing
+ *
+ * Primary upgrades:
+ *  1. 
+ *  2.
+ *  
+ * Optional extra upgrades:
+ *  3.
+ *  4.
+ *  
+ * Known Bugs: None
+ * 
+ * Other notes: N/A
+ * 
+ */
+namespace HW4_Arena
+{
+    /// <summary>
+    /// Primary class for the console app. Main() will be run on program launch. Other helper methods are
+    /// also defined that Main() will need. It's your job to finish them!
+    /// 
+    /// Do NOT change anything except where explicitly marked with a TODO comment!
+    /// See the comments through this program AND read the assignment write-up for details.
+    /// </summary>
+    internal class Program
+    {
+        // *** These constants are defined for you to make your code more readable AND help ensure it works
+        //     with the code given to you. Do NOT change these!
+
+        // Constants for the tile types
+        private const char Empty = ' ';
+        private const char Wall = '#';
+        private const char Enemy = 'E';
+        private const char Player = '@';
+        private const char PlayerStart = '0';
+        private const char Exit = '1';
+
+        // Constants for directions
+        private const char Up = 'w';
+        private const char Down = 's';
+        private const char Left = 'a';
+        private const char Right = 'd';
+
+        // Player stat indices
+        private const int Strength = 0;
+        private const int Dexterity = 1;
+        private const int Constitution = 2;
+        private const int Health = 3;
+
+        // Possible fight outcomes
+        private const int Win = 0;
+        private const int Lose = 1;
+        private const int Run = 2;
+        private const int Draw = 3;
+
+        // *** Other constants
+        // TODO: It's okay to tweak these numbers a bit to balance your game and/or add new ones.
+        // (But don't delete what is here. Main needs some of them!)
+        const int EnemySpacing = 6;
+        const int MaxPoints = 10;
+        const int HealthMult = 5;
+        const int DamageMult = 5;
+        const int EnemyAttack = 5;
+        const int EnemyMaxHealth = 50;
+
+        /// <summary>
+        /// DO NOT CHANGE ANY CODE IN MAIN!!!
+        /// 
+        /// But it's definitely worth reading it to get an understanding of 
+        /// how/when your methods will be called.
+        /// 
+        /// AND it's okay to *temporarily* comment out chunks of code until 
+        /// you're ready for them to run to make it easier to test other things.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            // ** SETUP **
+            // Player's name
+            string name;
+
+            // Stats - to make it easier to pass these around between methods, all 4 stats are
+            // in a single array with elements in the order [Strength, Dexterity, Constitution, Health]
+            // Constants are defined above to help with this.
+            int[] stats = new int[4];
+
+            // Define the variable to refer to the final arena
+            char[,] arena;
+
+            // Track the player's location as [row, col] (NOT x, y)
+            int[] playerLoc = {1, 1};
+
+            // Is the game still running?
+            bool stillPlaying = true;
+
+            // How many enemies are left?
+            int numEnemies;
+
+            // ** GET PLAYER STATS & BUILD ARENA **
+            // Welcome & get stats 
+            name = GetPlayerInfo(stats);
+
+            // Build & print the Arena
+            arena = BuildArena(out numEnemies);
+
+            // ** GAME LOOP **
+            while (stillPlaying)
+            {
+                // ** PRINT EVERYTHING **
+
+                // Clear the console and then print the arena
+                Console.Clear();
+                PrintArena(arena, playerLoc);
+                Console.WriteLine(
+                    $"\n{name}, your stats are: " +
+                    $"Strength {stats[Strength]}, " +
+                    $"Dexterity {stats[Dexterity]}, " +
+                    $"Constitution {stats[Constitution]}, " +
+                    $"Health {stats[Health]}");
+
+                // ** DETECT MOVEMENT **
+
+                // Get the desired direction
+                char direction = SmartConsole.GetPromptedChoice(
+                        $"\n Where would you like to go? {Up}/{Left}/{Down}/{Right} >",
+                        new char[] { Up, Left, Down, Right });
+                Console.WriteLine();
+
+                // Figure out what is there, but don't move yet
+                int[] nextLoc = { playerLoc[0], playerLoc[1] };
+                switch (direction)
+                {
+                    case Up:
+                        nextLoc[0]--; // row--
+                        break;
+
+                    case Down:
+                        nextLoc[0]++; // row++
+                        break;
+
+                    case Left:
+                        nextLoc[1]--; // col --
+                        break;
+
+                    case Right:
+                        nextLoc[1]++; // col ++
+                        break;
+                }
+
+                // ** TAKE ACTION **
+                // Act based on what is in the next location (row, col)
+                switch(arena[nextLoc[0], nextLoc[1]])
+                {
+                    // Do nothing. We're stuck.
+                    case Wall:
+                        Console.WriteLine("\n You can't go there...");
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+
+                    // Move to that spot
+                    case Empty:
+                        playerLoc = nextLoc;
+                        break;
+
+                    // Launch a new fight and determine how to proceed based on the result
+                    case Enemy:
+                        switch(Fight(stats))
+                        {
+                            // Take over the enemy's spot if we win
+                            case Win:
+                                playerLoc = nextLoc;
+                                arena[playerLoc[0], playerLoc[1]] = Empty;
+                                numEnemies--;
+                                break;
+
+                            // A loss or draw is game over
+                            case Lose:
+                            case Draw:
+                                stillPlaying = false;
+                                break;
+
+                            // Run back to the start and regain half health
+                            case Run:
+                                Console.WriteLine("You retreat to the starting area of the arena to heal up.");
+                                playerLoc = new int[] { 1, 1 };
+                                stats[Health] += (stats[Constitution] * HealthMult)/2;
+                                stats[Health] = Math.Clamp(stats[Health], 0, stats[Constitution] * HealthMult); // cap at max health
+                                break;
+                        }
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+
+                    case Exit:
+                        if(numEnemies > 0)
+                        {
+                            Console.WriteLine("You must defeat all enemies before you can escape.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("You made it to the exit! Congratulations!");
+                            stillPlaying = false;
+                        }
+                        Console.WriteLine("\nPress any key to continue.");
+                        Console.ReadKey();
+                        break;
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Given a reference to the player's current stats, launch a new fight
+        /// </summary>
+        /// <param name="stats">A reference to an int[] containing [Strength, Dexterity, Constitution, Health]</param>
+        /// <returns>The result of the fight using an int code. See the constants at the top of Program.cs</returns>
+        private static int Fight(int[] stats)
+        {
+            // TODO: Implement the Fight method
+            // ~~~~ YOUR CODE STARTS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            return 0; // replace this. it's just so the starter code compiles.
+            // ~~~~ YOUR CODE STOPS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+
+        /// <summary>
+        /// Get the player's name & stats. Stats are loaded into the provided array and
+        /// the name is returned.
+        /// </summary>
+        /// <param name="statsArray">A reference int[4] array that this method will put data into</param>
+        /// <returns>The player's name</returns>
+        private static string GetPlayerInfo(int[] statsArray)
+        {
+            // TODO: Implement the GetPlayerInfo method
+            // ~~~~ YOUR CODE STARTS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            return "???"; // replace this. it's just so the starter code compiles.
+            // ~~~~ YOUR CODE STOPS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+
+        /// <summary>
+        /// Given a reference to a 2d array variable (that will be null to start):
+        /// - Prompt for the desired size and initialize the array
+        /// - Put walls along all borders
+        /// - Evenly space enemies every few tiles (vert & hor)
+        /// - Put empty cells everywhere else
+        /// - Place the player start in the top left
+        /// - Place an exit in the bottom right
+        /// </summary>
+        /// <param name="numEnemies">An out param to store the total number of enemies created</param>
+        /// <returns>A reference to the final 2d arena</returns>
+        private static char[,] BuildArena(out int numEnemies)
+        {
+            // Start by setting numEnemies to 0. Increment this whenever you create
+            // an enemy and the out param will work just fine. :)
+            numEnemies = 0;
+
+            // TODO: Implement the BuildArena method
+            // ~~~~ YOUR CODE STARTS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            char[,] arena = null; // replace this. it's just so the starter code compiles.
+            // ~~~~ YOUR CODE STOPS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+            // All done
+            return arena;
+        }
+
+        /// <summary>
+        /// Given a reference to a 2d arena and the player's current location, 
+        /// print every character using the correct colors.
+        /// </summary>
+        /// <param name="arena">A reference to the arena to print. This could be ANY size.</param>
+        /// <param name="playerLoc">The player's location in a 1d array with element [row, col]</param>
+        private static void PrintArena(char[,] arena, int[] playerLoc)
+        {
+            // TODO: Implement the PrintArena method
+            // ~~~~ YOUR CODE STARTS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            // ~~~~ YOUR CODE STOPS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+    }
+}
---
+++ IGM206 HW/HW4_Arena/SmartConsole.cs
@@ -0,0 +1,87 @@
+﻿namespace HW4_Arena
+{
+    /// <summary>
+    /// Class to hold static helper methods related to prompting for 
+    /// and parsing, validating, and returning user responses.
+    /// 
+    /// Do NOT change anything except where explicitly marked with a TODO comment!
+    /// See the comments through this program AND read the assignment write-up for details.
+    /// </summary>
+    internal class SmartConsole
+    {
+        /// <summary>
+        /// Helper method to prompt the user to enter a number. If their
+        /// response isn't a valid int or isn't in the desired range, reprompt
+        /// </summary>
+        /// <param name="prompt">The string to use in the initial prompt</param>
+        /// <param name="min">The minimum accepted value (inclusive)</param>
+        /// <param name="max">The maximum accepted value (inclusive)</param>
+        /// <returns>The final, valid, user-entered value.</returns>
+        public static int GetValidIntegerInput(string prompt, int min, int max)
+        {
+            // TODO: Implement the GetValidIntegerInput method (you should be able to get this from your PE)
+            // ~~~~ YOUR CODE STARTS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+            return min; // replace this. it's just so the starter code compiles.
+            // ~~~~ YOUR CODE STOPS HERE ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        }
+
+        /// <summary>
+        /// Given a reference to an array of possible choices, keep prompting
+        /// the user until they enter a valid option
+        /// NOTE: Validation assumes lower case choices!
+        /// DO NOT TOUCH THIS METHOD!
+        /// </summary>
+        /// <param name="prompt">The prompt to use</param>
+        /// <param name="choices">The valid options</param>
+        /// <returns>The final valid choice</returns>
+        public static char GetPromptedChoice(string prompt, char[] choices)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write(prompt + " ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            char result = Console.ReadKey().KeyChar; // Get JUST 1 character
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // We haven't taught using Predicates in parameters. There are ways to implement
+            // this with what you've learned so far, but I didn't feel like making you worry about
+            // anything not related to exceptions or TryParse for this PE.
+            // https://learn.microsoft.com/en-us/dotnet/api/system.array.exists?view=net-7.0
+            while (!Array.Exists(choices, element => element == result))
+            {
+                Console.WriteLine("\nCommand not recognized.\n");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.Write("\n"+prompt + " ");
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                result = char.ToLower(Console.ReadKey().KeyChar); // Get JUST 1 character
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Input helper written by Prof. Mesh
+        /// Uses the given string to prompt the user for input and set
+        /// the color to cyan while they type.
+        /// DO NOT TOUCH THIS METHOD!
+        /// </summary>
+        /// <param name="prompt">What to print before waiting for input</param>
+        /// <returns>A trimmed version of what the user entered</returns>
+        public static string GetPromptedInput(string prompt)
+        {
+            // Always print in white
+            Console.ForegroundColor = ConsoleColor.White;
+
+            // Print the prompt
+            Console.Write(prompt + " ");
+
+            // Switch color and get user input (trim too)
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            string response = Console.ReadLine().Trim();
+
+            // Switch back to white and then return response.
+            Console.ForegroundColor = ConsoleColor.White;
+            return response;
+        }
+
+    }
+}
