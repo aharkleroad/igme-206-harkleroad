4a76a1cf829b1e31cdbf2385f4f09df8201edd40
aharkleroad <arh4811@g.rit.edu>
2024-10-10 21:21:53 -0500

    Final PE - File IO

---
+++ IGM206 PE/PE_FileIO_AHarkleroad/Player.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PE_FileIO_AHarkleroad
+{
+    internal class Player
+    {
+        // field declaration
+        private string name;
+        private int health;
+        private int strength;
+
+        // Player constructor
+        public Player(string name, int strength, int health)
+        {
+            this.name = name;
+            this.strength = strength;
+            this.health = health;
+        }
+
+        // Player properties
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int Health
+        {
+            get { return health; }
+        }
+
+        public int Strength
+        {
+            get { return strength; }
+        }
+
+        // returns a string of all of a player's stats, including name, strength, and health
+        public override string ToString()
+        {
+            return String.Format("Player: {0}. Strength {1}, Health {2}.", Name, Strength, Health);
+        }
+    }
+}
---
+++ IGM206 PE/PE_FileIO_AHarkleroad/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+ * Aizlynn Harkleroad
+ * 
+ * PE - File IO
+ * No known bugs or issues
+ */
+
+namespace PE_FileIO_AHarkleroad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // variable declaration
+            PlayerManager manager = new PlayerManager();
+            string input = "";
+
+            // until the player enters quit, prompt them for input
+            while (input != "quit")
+            {
+                Console.Write("Create, print, save, load, or quit? >> ");
+                input = Console.ReadLine().Trim().ToLower();
+
+                switch (input)
+                {
+                    // creates a Player object and adds it to the list
+                    case "create":
+                        string name;
+                        bool isInteger;
+                        int strength;
+                        int health;
+
+                        Console.Write("What is the player's name? ");
+                        name = Console.ReadLine().Trim();
+                        Console.Write("What is the player's strength? ");
+                        isInteger = int.TryParse(Console.ReadLine(), out strength);
+                        while (isInteger == false) 
+                        {
+                            Console.Write("Strength must be a whole number. What is the player's strength? ");
+                            isInteger = int.TryParse(Console.ReadLine(), out strength);
+                        }
+                        Console.Write("What is the player's health? ");
+                        isInteger = int.TryParse(Console.ReadLine(), out health);
+                        while (isInteger == false)
+                        {
+                            Console.Write("Health must be a whole number. What is the player's health? ");
+                            isInteger = int.TryParse(Console.ReadLine(), out health);
+                        }
+
+                        manager.CreatePlayer(name, strength, health);
+                        Console.WriteLine();
+                        break;
+                    // prints each player that has been created 
+                    case "print":
+                        manager.Print();
+                        Console.WriteLine();
+                        break;
+                    // saves all the current players to a file
+                    case "save":
+                        manager.Save();
+                        Console.WriteLine();
+                        break;
+                    // loads the players saved to the file to the list
+                    case "load":
+                        manager.Load();
+                        Console.WriteLine();
+                        break;
+                    // ends the program
+                    case "quit":
+                        Console.WriteLine("Goodbye!");
+                        break;
+                    // prints an error
+                    default:
+                        Console.WriteLine("Invalid input, please enter create, print, save, load, or quit.");
+                        Console.WriteLine();
+                        break;
+                }
+            }
+        }
+    }
+}
