8c63c6404adb8248e73fb03098d48eb54c30b76e
aharkleroad <arh4811@g.rit.edu>
2024-10-14 16:02:43 -0500

    Unfinished version of Homework 5
    SmartConsole and header added. No other code.

---
+++ IGM206 HW/HW_TheFarmstead_AHarkleroad/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+ * Aizlynn Harkleroad
+ * 
+ * HW5 - TheFarmstead
+ * https://docs.google.com/document/d/1xnF9pZIhLC-PW3OAOktW15VzMtAktTZWnHEsuQSDBpQ/edit?tab=t.0
+ * No known bugs or errors
+ */
+
+namespace HW_TheFarmstead_AHarkleroad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
