ec9f2ca75037f7c60b19edc4a593698ce2f70b88
aharkleroad <arh4811@g.rit.edu>
2024-10-22 13:49:53 -0400

    HW6 - Critter Farm, unfinished vers.

---
+++ IGM206 HW/HW6_CritterFarm_AHarkleroad/Critter.cs
@@ -0,0 +1,249 @@
+﻿// ------------------------------------------------------------------
+// DO NOT MODIFY ANY CODE IN THIS FILE EXCEPT:
+// - To add comments where marked with TODO questions
+// - To change the critter types available
+// ------------------------------------------------------------------
+namespace HW6_CritterFarm
+{
+
+    /// <summary>
+    /// The Critter class defines the core data tracked and behavior for any
+    /// critter, but there can be any number of critter types. You'll be required
+    /// to implement at least 3 (you can change these to something different if you
+    /// wish).
+    /// 
+    /// This is called an enumeration. You've seen them before (ConsoleColor
+    /// is an enum). You use them like constants by providing the type
+    /// and the value - e.g. CritterMood myMood = CritterType.Cat 
+    /// </summary>
+    // TODO: Add THREE new classes that inherit from the Critter base class
+    //       Make sure your critter child class names match the enum values below.
+    //       You may change these if you want. At least ONE class must have a method
+    //       that exists only in the child class. (Cat has CauseMischief in the demo)
+    public enum CritterType
+    {
+        Cat,
+        Dog,
+        Horse
+    }
+
+    /// <summary>
+    /// At any given time, a critter can be in one of these moods. It's
+    /// behavior will depend on this.
+    /// 
+    /// This is called an enumeration. You've seen them before (ConsoleColor
+    /// is an enum). You use them like constants by providing the type
+    /// and the value - e.g. CritterMood myMood = CritterMood.Happy 
+    /// </summary>
+    public enum CritterMood
+    {
+        Happy,
+        Frustrated,
+        Angry
+    }
+
+    /// <summary>
+    /// TODO: Add your own summary of this class!
+    /// </summary>
+    abstract class Critter
+    {
+        // ----------------------------------------------------------------------
+        // CONSTANTS
+        // You may change the values ONLY.
+        // ----------------------------------------------------------------------
+
+        // Some constants to make quickly changing the default
+        // behavior/game balance a bit simpler.
+        protected const int FoodAmount = 4;
+        protected const int FunAmount = 4;
+        protected const int GenAngryLvl = 25;
+        protected const int GenFrustrationLvl = 15;
+
+        // ----------------------------------------------------------------------
+        // Fields
+        // ----------------------------------------------------------------------
+
+        // The data that all critters have.
+        protected string name;
+        protected CritterMood mood;
+        protected CritterType type;
+        private int hungerLevel;
+        private int boredomLevel;
+
+        // ----------------------------------------------------------------------
+        // Properties
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// Read-only public access to the critter type
+        /// </summary>
+        public CritterType Type { get { return type; } }
+
+        /// <summary>
+        /// Read-only public access to the critter's name
+        /// </summary>
+        public String Name { get { return name; } }
+
+        /// <summary>
+        /// Read-only public access to the critter's hunger level, but
+        /// let child classes set the value (but not below 0)
+        /// </summary>
+        public int Hunger 
+        { 
+            get { return hungerLevel; } 
+            protected set
+            {
+                hungerLevel = value;
+
+                // Never go below 0
+                if(hungerLevel < 0)
+                {
+                    hungerLevel = 0;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Read-only public access to the critter's boredom level, but
+        /// let child classes set the value (but not below 0)
+        /// </summary>
+        public int Boredom
+        {
+            get { return boredomLevel; }
+            protected set
+            {
+                boredomLevel = value;
+
+                // Never go below 0
+                if (boredomLevel < 0)
+                {
+                    boredomLevel = 0;
+                }
+            }
+        }
+
+        // ----------------------------------------------------------------------
+        // Constructor
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// The Critter class defines only 1 constructor. It is defined as protected so that
+        /// it can only be used via a child class (calling it via base(...))
+        /// Do NOT add a default constructor and do NOT change this to public
+        /// </summary>
+        /// <param name="name">All critters must be created with a name</param>
+        /// <param name="type">All critters must be created with a type</param>
+        /// <param name="hungerLevel">This is defined as an optional parameter. 
+        /// If not provided, the hunger level of the new critter defaults to 0</param>
+        /// <param name="boredonLevel">This is defined as an optional parameter. 
+        /// If not provided, the boredom level of the new critter defaults to 0</param>
+        protected Critter(string name, CritterType type, int hungerLevel = 0, int boredomLevel = 0)
+        {
+            this.name = name;
+            this.type = type;
+            this.hungerLevel = hungerLevel;
+            this.boredomLevel = boredomLevel;
+            UpdateMood();
+        }
+
+        // ----------------------------------------------------------------------
+        // Methods
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// Updates mood variable based on current happiness level.
+        /// Called whenever time passes.
+        /// 
+        /// TODO: Add a comment explaining why it makes sense for this method
+        ///         to be abstract, but still defined here in the parent class.
+        /// </summary>
+        protected abstract void UpdateMood();
+
+        /// <summary>
+        /// Feeds a critter so its hunger level will reduce.
+        /// Lower levels indicate a happier critter.
+        /// </summary>
+        public void Eat()
+        {
+            Console.WriteLine("{0} eats a bit. Brrrp!", name);
+
+            // Decrease hunger level, but never beyond 0
+            hungerLevel -= FoodAmount;
+            if(hungerLevel < 0)
+            {
+                hungerLevel = 0;
+            }
+        }
+
+
+        /// <summary>
+        /// Plays with critter so its boredom level will reduce.
+        /// Lower levels indicate a happier critter.
+        /// </summary>
+        public void Play()
+        {
+            Console.WriteLine("{0} loves playing. Wheee!", name);
+
+            // Decrease boredom level, but never beyond 0
+            boredomLevel -= FunAmount;
+            
+            if (boredomLevel < 0)
+            {
+                boredomLevel = 0;
+            }
+        }
+
+        /// <summary>
+        /// Get a visual indication of how this critter feels.
+        /// Prints a verbal statement of feelings with an emoticon.
+        /// </summary>
+        public void Talk()
+        {
+            // Print this critter's feelings based on the set enum variable.
+            Console.Write("{0} feels ", name);
+            switch(mood)
+            {
+                case CritterMood.Happy:
+                    Console.WriteLine("happy with a full belly and an engaged mind! :)");
+                    break;
+                case CritterMood.Frustrated:
+                    Console.WriteLine("frustrated; being hungry and bored is not fun. :|");
+                    break;
+                case CritterMood.Angry:
+                    Console.WriteLine("angry! Critter smash! Please feed and play with the critter! >:(");
+                    break;
+            }
+        }
+
+
+        /// <summary>
+        /// Simulates passing of time.
+        /// As time progresses, critters become slightly more hungry
+        /// and bored. 
+        /// </summary>
+        public void PassTime()
+        {
+            hungerLevel++;
+            boredomLevel++;
+            UpdateMood();
+        }
+
+        /// <summary>
+        /// Provides a string representation of the critter's current stats
+        /// </summary>
+        public override string ToString()
+        {
+            // Make sure the mood is up to date
+            UpdateMood();
+
+            // Build a string summarizing this critter
+            return String.Format(
+                "{0} is {3} (Hunger: {1}, Boredom {2})",
+                name.ToUpper(),
+                hungerLevel,
+                boredomLevel,
+                mood.ToString().ToLower()); // Converts the enum to an all lowercase string
+        }
+
+    }
+}
---
+++ IGM206 HW/HW6_CritterFarm_AHarkleroad/CritterManager.cs
@@ -0,0 +1,303 @@
+﻿// ------------------------------------------------------------------
+// DO NOT MODIFY ANY CODE IN THIS FILE EXCEPT:
+// - To implement file loading & saving
+// - To change the critter types available
+// ------------------------------------------------------------------
+
+namespace HW6_CritterFarm
+{
+    /// <summary>
+    /// TODO: Add your own summary of this class!
+    /// </summary>
+    class CritterManager
+    {
+        // ----------------------------------------------------------------------
+        // Fields
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// List to hold all of the critters by leveraging polymorphism
+        /// </summary>
+        private List<Critter> critterList;
+
+        /// <summary>
+        /// Reference to the current active critter
+        /// </summary>
+        private Critter activeCritter;
+
+        /// <summary>
+        /// The filename to use when loading/saving critters
+        /// </summary>
+        private string filename;
+
+        /// <summary>
+        /// Generator for any pseuod-random numbers needed
+        /// </summary>
+        private Random rng = new Random();
+
+        // ----------------------------------------------------------------------
+        // Properties
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// Return the NAME of the current active critter or return null if
+        /// one isn't active
+        /// </summary>
+        public String ActiveCritter 
+        {
+            get 
+            {
+                if (activeCritter != null)
+                {
+                    return activeCritter.Name;
+                }
+                else
+                {
+                    return null!; // The ! tells the compiler to ignore warnings about a possible null value.
+                }
+            } 
+        }
+
+        // ----------------------------------------------------------------------
+        // Default Constructor
+        // ----------------------------------------------------------------------
+
+        /// <summary>
+        /// Default constructor to initialize the list and active critter
+        /// </summary>
+        public CritterManager(string filename)
+        {
+            critterList = new List<Critter>();
+            activeCritter = null!; // The ! tells the compiler to ignore warnings about a possible null value.
+            this.filename = filename;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+        // Critter setup via user input
+        // ---------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Initial setup of critters. 
+        /// Prompts a user for a number of critters, then their names,
+        /// then adds newly instantiated critters to the critter list.
+        /// All critters start with 0 hunger and 0 boredom.
+        /// 
+        /// TODO: Review SetupCritters() 
+        /// THIS METHOD IS DONE FOR YOU so that you have examples of how to TryParse an enum type and
+        /// use a switch statement with enums. You'll have to do both yourself when loading from a file!
+        /// </summary>
+        public void SetupCritters()
+        {
+            // Ask user for number of critters and their names
+            int numberCritters = SmartConsole.GetValidNumericInput(
+                "How many critters should your farm contain (1-5)?",
+                1, 5);
+
+            // Gather information about critter names from user,
+            //   then create new Critter into the list
+            for (int i = 0; i < numberCritters; i++)
+            {
+                string name = SmartConsole.GetPromptedInput("\nEnter critter " + (i + 1) + " name:");
+                string typeString = SmartConsole.GetPromptedInput("What type of critter is "+name+" (Cat, Dog, or Horse)?");
+                CritterType type = CritterType.Cat;
+
+                // Enums work with a TryParse too! :)
+                while (!Enum.TryParse<CritterType>(typeString, true, out type) // true as the middle param tells TryParse to ignore case
+                    || !Enum.IsDefined(typeof(CritterType), type))
+                        // TryParse accepts ints that aren't actually valid for this
+                        // enum. Using IsDefined checks them before allowing the loop
+                        // to proceed.
+                {
+                    SmartConsole.PrintWarning("Sorry, I don't know how to take care of a "+typeString+".\n");
+                    typeString = SmartConsole.GetPromptedInput("What type of critter is " + name + " (Cat, Dog, or Horse)?");
+                }
+
+                // Create the correct type of critter
+                // TODO: If you don't use Cat, Dog, & Horse as your critter types, fix the setup method
+
+                // The switch statement cases and constructor calls below need to match YOUR critter types
+                // TODO: Uncomment this once your child classes exist.
+                /*
+                switch (type)
+                {
+                    case CritterType.Cat:
+                        critterList.Add(new Cat(name));
+                        break;
+
+                    case CritterType.Dog:
+                        critterList.Add(new Dog(name));
+                        break;
+
+                    case CritterType.Horse:
+                        critterList.Add(new Horse(name));
+                        break;
+
+                    default:
+                        // shouldn't happen
+                        SmartConsole.PrintError(String.Format("Not sure how to create {0} with a type of {1}", name, typeString));
+                        i--; // Didn't actually add a critter so go back 1 with our lcv and try again.
+                        break;
+                }
+                */
+            }
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+        // Critter file loading and saving
+        // ---------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Loads critter data from a file
+        /// If there are no saved critters, calls SetupCritters() to allow user to
+        /// enter their own critters.
+        /// If file data exists, populates critterList with critters built from
+        /// the file's data.
+        /// </summary>
+        public void LoadCrittersFromFile()
+        {
+            // ********************************
+            // File name: critters.txt
+            // File structure (sample line):
+            // type|name|hunger #|boredom #
+            // ********************************
+
+            // TODO: Implement LoadCrittersFromFile()
+        }
+
+
+        /// <summary>
+        /// Saves critter data to a file.
+        /// If there are no saved critters, writes "No critters saved."
+        /// If critters do exist, writes their data to the file.
+        /// </summary>
+        public void SaveCrittersToFile()
+        {
+            // ********************************
+            // File name: critters.txt
+            // File structure (sample line):
+            // type|name|hunger #|boredom #
+            // ********************************
+
+            // TODO: Implement SaveCrittersToFile()
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+        // Critter Control Methods
+        // ---------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Sets the active critter to one of the critters in the critter list.
+        /// </summary>
+        /// <param name="critterName">Name of critter to set the active critter to</param>
+        /// <returns>Whether this operation was successful</returns>
+        public bool ChooseCritter(string critterName)
+        {
+            // Determine if the Critter exists in the list
+            // And set it as active
+            activeCritter = null!; // The ! tells the compiler to ignore warnings about a possible null value.
+            for (int i = 0; i < critterList.Count; i++)
+            {
+                if (critterList[i].Name == critterName)
+                {
+                    activeCritter = critterList[i];
+                }
+            }
+
+            // Let Main know whether this was successful
+            return activeCritter != null;
+        }
+
+        /// <summary>
+        /// Retrieves a list of the names of all current critters
+        /// </summary>
+        /// <returns>List of names of all critters</returns>
+        public List<string> GetCritterNames()
+        {
+            List<string> critterNames = new List<string>();
+            for (int i = 0; i < critterList.Count; i++)
+            {
+                critterNames.Add(critterList[i].Name);
+            }
+            return critterNames;
+        }
+
+        // ---------------------------------------------------------------------------------------------------------------
+        // Critter Actions
+        // ---------------------------------------------------------------------------------------------------------------
+
+        /// <summary>
+        /// Feeds the active critter 4 units of food.
+        /// </summary>
+        public void FeedCritter()
+        {
+            if (activeCritter == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Feeding your critter...");
+            activeCritter.Eat();
+        }
+
+
+        /// <summary>
+        /// Plays with the active critter for 4 fun units.
+        /// </summary>
+        public void PlayWithCritter()
+        {
+            if (activeCritter == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Playing with your critter...");
+            activeCritter.Play();
+        }
+
+
+        /// <summary>
+        /// Talks with the active critter.
+        /// </summary>
+        public void TalkToCritter()
+        {
+            if (activeCritter == null)
+            {
+                return;
+            }
+
+            Console.WriteLine("Talking to your critter...");
+            activeCritter.Talk();
+        }
+
+
+        /// <summary>
+        /// Simulates time passing for every critter for every "round" of user actions.
+        /// </summary>
+        public void TimePassing()
+        {
+            foreach (Critter c in critterList)
+            {
+                c.PassTime();
+                // TODO: Update this to call any child specific methods as well.
+                //       For example, in the demo, time passing calls the
+                //       the CauseMischeif method on any cats 25% of the time
+            }
+        }
+
+        /// <summary>
+        /// Prints critter data about every critter in the list.
+        /// Helpful for testing. 
+        /// </summary>
+        public void PrintCritters()
+        {
+            // Get string representation of every Critter in the list
+            for(int i = 0; i < critterList.Count; i++)
+            {
+                Console.WriteLine("{0} ({2}): {1}", 
+                    i + 1,
+                    critterList[i],
+                    critterList[i].Type);
+            }
+        }
+    }
+}
